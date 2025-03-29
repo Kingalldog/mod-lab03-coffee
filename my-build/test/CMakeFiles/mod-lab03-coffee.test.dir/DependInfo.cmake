5df23c40fad15b68c47565a48aa6e013ffd5fadb
Kingalldog <vsamohin@huskyeg>
2025-03-29 14:49:34 +0300

    fix time

---
+++ my-build/test/CMakeFiles/mod-lab03-coffee.test.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/vsamohin/mod-lab03-coffee/test/AllTests.cpp" "/home/vsamohin/mod-lab03-coffee/my-build/test/CMakeFiles/mod-lab03-coffee.test.dir/AllTests.cpp.o"
+  "/home/vsamohin/mod-lab03-coffee/test/tests.cpp" "/home/vsamohin/mod-lab03-coffee/my-build/test/CMakeFiles/mod-lab03-coffee.test.dir/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "TEST_DIR=\"/home/vsamohin/mod-lab03-coffee/test/test\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "src"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/vsamohin/mod-lab03-coffee/my-build/src/CMakeFiles/mod-lab03-coffee.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
